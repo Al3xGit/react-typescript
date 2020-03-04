2654f03be1373c0efe6d20d275cbe2c13e1460c0
alex <alexandre.meur@protonmail.com>
2020-03-04 21:49:03 +0100

    basic clock done

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,15 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component, createRef, useRef } from 'react'
 import CSS from 'csstype'
 
-export default class Clock extends Component {
+interface ClockState { date: Date }
+
+export default class Clock extends Component<{}, ClockState> {
+
+  constructor(props: {}) {
+    super(props)
+    this.state = { date: new Date() }
+    setInterval(this.tick, 1000)
+  }
+
+  tick = () => {
+    this.setState({
+      date: new Date()
+    })
+  }
 
   render(): JSX.Element {
     return (
       <div style={clockStyle}>
-        <p>clock</p>
+        <p>il est {this.state.date.toLocaleTimeString()}</p>
       </div>
     )
   }
+
 }
 
 const clockStyle: CSS.Properties = {
